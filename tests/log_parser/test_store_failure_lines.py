94ebeca251089ad374242f68be52f045e101d99e
James Graham <james@hoppipolla.co.uk>
2016-05-03 15:31:05 +0100

    Bug 1268484 - Add elastic-search based matcher for test failure lines
    Add support for matching test failures where the test, subtest, status,
    and expected status are all exact matches, but the message is not an
    exact match. The matching uses ElasticSearch and is initially optimised
    for cases where the messages differ only in numeric values since this is
    a relatively common case.
    
    This commit also adds ElasticSearch to the travis environment.

--- tests/log_parser/test_store_failure_lines.py
+++ tests/log_parser/test_store_failure_lines.py
@@ -5,6 +5,7 @@ from treeherder.log_parser.failureline import store_failure_lines
 from treeherder.model.models import (FailureLine,
                                      Job,
                                      JobLog)
+from treeherder.model.search import TestFailureLine
 
 from ..sampledata import SampleData
 
@@ -87,3 +88,29 @@ def test_store_error_summary_astral(activate_responses, test_repository, jm,
     assert failure.stack.endswith("<U+0F0151>")
     assert failure.stackwalk_stdout is None
     assert failure.stackwalk_stderr is None
+
+
+def test_store_error_summary_elastic_search(activate_responses, test_repository,
+                                            jm, eleven_jobs_stored, elasticsearch):
+    log_path = SampleData().get_log_path("plain-chunked_errorsummary.log")
+    log_url = 'http://my-log.mozilla.org'
+
+    with open(log_path) as log_handler:
+        responses.add(responses.GET, log_url,
+                      body=log_handler.read(), status=200)
+
+    job = Job.objects.get(guid=jm.get_job(1)[0]['job_guid'])
+
+    log_obj = JobLog.objects.create(job=job, name="errorsummary_json", url=log_url)
+
+    store_failure_lines(jm.project, job.guid, log_obj)
+
+    assert FailureLine.objects.count() == 1
+
+    failure = FailureLine.objects.get(pk=1)
+
+    es_line = TestFailureLine.get(1)
+    for prop in ["test", "subtest", "status", "expected"]:
+        assert getattr(es_line, prop) == getattr(failure, prop)
+    assert es_line.best_classification is None
+    assert es_line.best_is_verified is False
